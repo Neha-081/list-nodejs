f44ce24d6e68c336b07d8671ef2703085708b2fe
Neha <nehaprajapati1150@gmail.com>
2021-12-15 23:30:04 +0530

    final

--- index.js
+++ index.js
@@ -2,17 +2,20 @@
 
 
 //const fs is now a object which have all the functionality includes in file sysytem
-const fs = require('fs');   //file system at  node
+const fs = require('fs');   //local file system at  node
 const util=require('util');   //get all info of file
-const chalk = require('chalk')
+const chalk = require('chalk');  //for giving colors to filename and folders
+const path = require('path');   
 
 const {lstat}=fs.promises;
+const targetDir=process.argv[2] || process.cwd();  //argv-array of info how program was executed
+
 
 //fs.readdir(path[,options],callback) 
 // where callback have 2 arguements i.e-(err,files)
  //readir reads the content of the directory
  //cwd-path of (current working directory)
-fs.readdir(process.cwd(),async(err,filenames)=>{       
+fs.readdir(targetDir,async(err,filenames)=>{       
   //EITHER
   //err=== an error object,which means somthing went wrong
   //OR
@@ -23,7 +26,8 @@ fs.readdir(process.cwd(),async(err,filenames)=>{
        console.log(err);
     }
     const statPromises=filenames.map(filename=>{
-        return lstat(filename)
+        return lstat(path.join(targetDir,filename))   //join the target folder and appended file,by this we can see all folders and file anywhere by nls ..
+        //stat of that file in current directory
     });
  const allStats= await Promise.all(statPromises)
 for(let stats of allStats){
@@ -37,3 +41,9 @@ if(stats.isFile()){
 }
 }   
 });  
+
+
+
+
+//dependencies-
+//chalk
