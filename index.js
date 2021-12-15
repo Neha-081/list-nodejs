66351511b2656144bbfa08939002aac14a180ff6
Neha <nehaprajapati1150@gmail.com>
2021-12-15 22:14:27 +0530

    lstat

--- index.js
+++ index.js
@@ -2,13 +2,17 @@
 
 
 //const fs is now a object which have all the functionality includes in file sysytem
-const fs = require('fs')   //file system at  node
+const fs = require('fs');   //file system at  node
+const util=require('util');   //get all info of file
+
+
+const {lstat}=fs.promises;
 
 //fs.readdir(path[,options],callback) 
 // where callback have 2 arguements i.e-(err,files)
  //readir reads the content of the directory
  //cwd-path of (current working directory)
-fs.readdir(process.cwd(),(err,filenames)=>{       
+fs.readdir(process.cwd(),async(err,filenames)=>{       
   //EITHER
   //err=== an error object,which means somthing went wrong
   //OR
@@ -18,5 +22,19 @@ fs.readdir(process.cwd(),(err,filenames)=>{
       //error handling code
        console.log(err);
     }
+    const statPromises=filenames.map(filename=>{
+        return lstat(filename)
+    });
+ const allStats= await Promise.all(statPromises)
+for(let stats of allStats){
+const index=allStats.indexOf(stats);
+
+console.log(filenames[index],stats.isFile());
+}
+    
+
+
+
+
      
 });  
